6bb3fb51d0c150a93ea5b0e26698c7881fa9f9ca
GiulianaCDA <giulianaarecippo@hotmail.com>
2021-11-28 02:40:58 -0300

    adiciona arquivos

--- simulacao.py
+++ simulacao.py
@@ -5,11 +5,30 @@ import pygame.freetype
 
 pygame.init()
 
+def home():
+    text_surface, rect = myfont.render("Bem vindo ao", (255, 255, 255))
+    screen.blit(text_surface, (430, 140))
+
+    text_surface, rect = myfont.render("caixa eletrônico", (255, 196, 224))
+    screen.blit(text_surface, (350, 200))
+
+    text_surface, rect = playfont.render("iniciar ", (255, 196, 224))
+    screen.blit(text_surface, (580, 420))
+
+    text_surface, rect = playfont.render("simulação", (255, 196, 224))
+    screen.blit(text_surface, (580, 465))
+
+    pygame.display.update() 
+
 #DISPLAY WINDOW
 sizeBackground = width, height = 1300, 650
 screen = pygame.display.set_mode(sizeBackground)
 background = pygame.image.load("background.png")
 background = pygame.transform.scale(background, (width, height))
+
+background2 = pygame.image.load("background3.png") 
+background2 = pygame.transform.scale(background2, (width, height))
+
 screen.blit(background, (0,0))
 
 #FONT
@@ -27,34 +46,22 @@ text = myfont.render(' play ' , True , (255,255,255))
 handCursor = pygame.SYSTEM_CURSOR_HAND   
 arrowCursor = pygame.SYSTEM_CURSOR_ARROW  
 
-def home():
-
-    text_surface, rect = myfont.render("Bem vindo ao", (255, 255, 255))
-    screen.blit(text_surface, (430, 140))
-
-    text_surface, rect = myfont.render("caixa eletrônico", (255, 196, 224))
-    screen.blit(text_surface, (350, 200))
-
-    text_surface, rect = playfont.render("iniciar ", (255, 196, 224))
-    screen.blit(text_surface, (580, 420))
-
-    text_surface, rect = playfont.render("simulação", (255, 196, 224))
-    screen.blit(text_surface, (580, 465))
-
-    pygame.display.update() 
+home()
     
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: exit()
         pygame.display.flip()
     
-    home()
-    
     mouse = pygame.mouse.get_pos()
+    
+    # Controla o botão de play
     if playbutton.collidepoint(mouse):
         pygame.mouse.set_cursor(handCursor)
-          
+        if event.type == MOUSEBUTTONDOWN:
+
+            screen.blit(background2, (0,0))
     else:
         pygame.mouse.set_cursor(arrowCursor)
         
-    pygame.display.update() 
+    pygame.display.update()
